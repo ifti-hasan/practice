f62333992a795ba9f3f90be7a670f5bb94ba02d1
iftikhar <iftiiihasan@gmail.com>
2023-05-09 20:46:59 +0600

    changed to useReducer

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,5 +1,8 @@
 import { useState,useReducer } from 'react'
 
+const increment = 'increment'
+const decrement = 'decrement'
+
 const initialState = { count: 0 }
 function reducer(state, action){
   switch(action.type){
@@ -14,28 +17,10 @@ function reducer(state, action){
 }
 
 function App() {
-  // const [count, setCount] = useState(0)
   const [state, dispatch ] = useReducer(reducer, initialState)
-
-  // const addCount = ()=>{
-  //   setCount(prevCount=>prevCount+1)
-  // }
-
-  // const deleteCount = ()=>{
-  //   setCount(prevCount=>{
-  //     if(prevCount === 0){
-  //       return 0
-  //     }
-  //     return prevCount-1
-  //   })
-  // }
 
   return (
     <div className='counter-app'>
-      {/* <div>
-        <h3 className='add' onClick={addCount}>add</h3>
-        <h3 className='subtract' onClick={deleteCount}>subtract</h3>
-      </div> */}
       <div>
         <h3 className='add' onClick={()=> dispatch({ type: 'increment', payload:1 })}>add</h3>
         <h3 className='subtract' onClick={()=> dispatch({ type: 'decrement', payload: 1 })}>subtract</h3>
